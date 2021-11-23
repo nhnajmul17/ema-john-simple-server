2ab2e94421f202104e7c4ee787a52d9cce663fab
nhnajmul17 <nhnajmul17@gmail.com>
2021-11-23 19:43:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+var admin = require("firebase-admin");
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//firebase admin initialize
+
+
+var serviceAccount = require("./ema-john-simple-0001-firebase-adminsdk-1tpwr-a675b41ffe.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zfz5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers.authorization?.startsWith('Bearer')) {
+        const idToken = req.headers.authorization.split('Bearer ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(idToken)
+            req.decodedUserEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+
+    next();
+}
+
+async function run() {
+
+    try {
+        await client.connect();
+        const database = client.db("ema_john");
+        const productCollection = database.collection("products");
+        const orderCollection = database.collection("orders")
+
+        //GET Products APi
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({})
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products;
+            const count = await cursor.count();
+
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+            res.send({
+                count,
+                products
+            })
+
+        })
+
+        //used PST to get data from keys
+        app.post('/products/bykeys', async (req, res) => {
+            const keys = (req.body);
+            const query = { key: { $in: keys } }
+            const products = await productCollection.find(query).toArray();
+            res.json(products)
+        })
+
+        //Get Ordesrs APi
+        app.get('/orders', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            if (req.decodedUserEmail === email) {
+
+                const query = { email: email };
+
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.json(orders)
+            }
+            else {
+                res.status(401).json({ message: 'User Not authorized' })
+            }
+
+
+
+
+        })
+        //ADD Ordesrs APi
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body
+            order.createdAt = new Date();
+            const result = await orderCollection.insertOne(order);
+            res.json(result)
+        })
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('This is Ema john Server')
+})
+
+app.listen(port, () => {
+    console.log('Running on port', port);
+})
